11220caf58251756009d33ee5c3c87b86eda2b70
duckun2512 <65001215+duckun2512@users.noreply.github.com>
2021-10-31 05:08:57 -0400

    update

--- reactuser/src/components/Payment/PaymentForm.js
+++ reactuser/src/components/Payment/PaymentForm.js
@@ -40,8 +40,9 @@ export default function PaymentForm(){
     if(!error){
         try{
             const {id} = paymentMethod
-            const response = await axios.post("http://localhost:4000/payment", {
+            const response = await axios.post("http://localhost:3000/payment", {
                 amount: 1000,
+                id
             })
 
             if (response.data.success){
@@ -52,9 +53,9 @@ export default function PaymentForm(){
             console.log("Error",error)
         }
     }else {
-        console.log(error.message)
+        console.log(error.message) 
     }
-    }
+}
 
     return (
         <>
