34fba8023b546016e52ad68a2c5583b803e39086
duckun2512 <65001215+duckun2512@users.noreply.github.com>
2021-11-02 06:07:40 -0400

    update

--- payment1/src/Components/StripeContainer.js
+++ payment1/src/Components/StripeContainer.js
@@ -6,7 +6,7 @@ import PaymentForm from "./PaymentForm";
 
 const PUBLIC_KEY= "pk_test_51JpPHIB2z1KbtnvFqoH49sQHvs5b9dso3Cv6uezHoqw2LuHEQqt3FXymLrAswRK8LaE8Doq3WjMpTpmFyRJ5i68G00i6YD008s"
 
-const stripeTestPromise =loadStripe(PUBLIC_KEY)
+const stripeTestPromise = loadStripe(PUBLIC_KEY)
 
 export default function StripeContainer(){
     return (
