cb8aadfd97187367c8b2589e3096bca30ec8366e
duckun2512 <65001215+duckun2512@users.noreply.github.com>
2021-11-09 06:31:07 -0500

    update

--- reactuser/src/components/Header/index.js
+++ reactuser/src/components/Header/index.js
@@ -6,7 +6,6 @@ const Header = () => {
 
 	return (
 		<header>
-<<<<<<< HEAD
 			<div className="header-main center">
 				<div className="header-menu">
 					<img src={images.logo} alt="Logo" width="70" />
@@ -36,19 +35,6 @@ const Header = () => {
 								About
 							</Link>
 						</li>
-=======
-			<div className='header-main'>
-				<div className='header-menu'>
-					<img src={images.Homepage.banner} alt="" width='70' height='50'/>
-					<ul className='header-nav'>
-						<li><Link className='header__a' to='' >Trang chu</Link></li>
-						<li><Link className='header__a' to=''>Nghe Si</Link></li>
-						<li><Link className='header__a' to=''>Tac pham</Link></li>
-						<li><Link className='header__a' to=''>Contact</Link></li>
-						<li><Link className='header__a' to='/about-us'>About</Link></li>
-						<button type='button'  className='btn-header'><span>Dang nhap</span></button>
-
->>>>>>> a47bafd135e6c24dac455979847ce9094da63d88
 					</ul>
 					<button type="button" className="btn-header" onClick={loginHandler}>
 						<span>Dang nhap</span>
