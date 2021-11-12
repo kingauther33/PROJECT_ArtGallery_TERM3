62971134bebe9a96ffda8cda9b134971d05544b4
NguyenTrongSon1 <sonntth2003006@fpt.edu.vn>
2021-11-12 10:05:13 +0700

    update register

--- reactuser/src/pages/Login/Register.js
+++ reactuser/src/pages/Login/Register.js
@@ -1,36 +1,38 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const Register = () => {
     return (
-        <>
-            <div className="wrapper">
-                <h2>Register</h2>
-                <form action="#">
+        <div className="register">
+            <div className="register_main">
+                <h2 className="register_text">Registration</h2>
+                <form action="#" className="register_form">
                     <div className="input-box">
-                        <input type="text" placeholder="Enter your name" required/>
+                        <input className="register-input" type="text" placeholder="Enter your name" required/>
                     </div>
                     <div className="input-box">
-                        <input type="text" placeholder="Enter your email" required/>
+                        <input className="register-input" type="text" placeholder="Enter your email" required/>
                     </div>
                     <div className="input-box">
-                        <input type="password" placeholder="Enter your password" required/>
+                        <input className="register-input" type="password" placeholder="Create password" required/>
                     </div>
                     <div className="input-box">
-                        <input type="password" placeholder="Confirm your password" required/>
+                        <input className="register-input" type="password" placeholder="Confirm password" required/>
                     </div>
                     <div className="policy">
-                        <input type="checkbox">
-                            <h3>I accept all terms and condittion</h3></input>
+                        <input  type="checkbox"/>
+                        <h3 className="register-h3">I accept all terms & condition</h3>
+
                     </div>
                     <div className="input-box button">
-                        <input type="submit" value="Register now"/>
+                        <input className="register-input register-box" type="Submit" value="Register Now"/>
                     </div>
                     <div className="text">
-                        <h3>Already have an account? <a href="#">Login now</a></h3>
+                        <h3 className="register-h3-text register-h3">Already have an account? <Link className="register-link" to='/'>Login now</Link></h3>
                     </div>
                 </form>
             </div>
-        </>
+        </div>
     );
 }
 export default Register;
